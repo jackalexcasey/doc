191b9b1b5fc6f0bfad9575c61326dff3efd03f44
Etienne <etmartin@etmartin-desktop.(none)>
2012-04-05 23:08:17 -0400

    robust mutex test cases; quick doc update

--- linux/notes/robust_mutex/main.c
+++ linux/notes/robust_mutex/main.c
@@ -18,6 +18,13 @@
 #define _POSIX_PATH_MAX 256
 #endif
 
+/* need that for the macro... */
+#define PAGE_SIZE 4096
+
+#define __ALIGN_MASK(x,mask)    (((x)+(mask))&~(mask))
+#define ALIGN(x,a)      __ALIGN_MASK(x,(typeof(x))(a)-1)
+#define PAGE_ALIGN(addr) ALIGN(addr, PAGE_SIZE)
+
 #define HWID_TABLE_SIZE (1<<20) /* 1Mb */
 #define MAX_MUTEX_NR (int)(HWID_TABLE_SIZE/sizeof(pthread_mutex_t))
 
@@ -30,6 +37,12 @@ struct sharedmutex{
 	pthread_mutex_t mutex[MAX_MUTEX_NR];
 };
 static int mutexnr=1;
+struct pages{
+	unsigned long *page;
+};
+
+
+int chewmem_main(int argc, char **argv);
 
 static inline void* mmap_hwid_file(void)
 {
@@ -81,23 +94,51 @@ void init(void)
 		pthread_mutex_init(&shmmutex->mutex[x], &attr);
 		pthread_mutex_lock(&shmmutex->mutex[x]);
 	}
-	while(1) sleep(1);
+#if 0
+	{
+		char *arg1 = "progname";
+		char *arg2 = "10000";
+		char *arg[] = {arg1,arg2,NULL};
+		chewmem_main(2, arg);
+	}
+#endif
+
+	/* Burn CPU to force the schedule uppon robust list release */
+	while(1){} 
 }
 
-void lock(int nr)
+void lock(int nr,int meg)
 {
-	int rc;
 	struct sharedmutex *shmmutex;
+	int rc,i;
+	unsigned char *ptr;
+	unsigned long size = 1024*1024*meg;
+	int pagesize = getpagesize();
 
-	fprintf(stderr,"LOCK %d\n",nr);
+	fprintf(stderr,"INIT %d %lx meg\n",nr,size);
 	shmmutex =  mmap_hwid_file();
 	if(!shmmutex){
 		perror("mmap_hwid_file");
 		return;
 	}
+
+	ptr = malloc(size);
+	if(!ptr){
+		fprintf(stderr,"Cannot allocate memory\n");
+		return -1;
+	}
+
 	rc = pthread_mutex_lock(&shmmutex->mutex[nr]);
-	if(rc == EOWNERDEAD)
+	if(rc == EOWNERDEAD){
 		fprintf(stderr,"pthread_mutex_lock EOWNERDEAD\n");
+		for (i=0; i<size; i=i+pagesize) {
+			ptr[i] = 1;
+//			fprintf(stderr,".");
+		}
+	}
+
+	while(1)
+		sleep(1);
 }
 
 void usage()
@@ -131,8 +172,95 @@ int main (int argc, char*argv[])
 		FATAL("could not truncate shared file\n");
 	}
 
+#if 0
+	/* This is the naming convention */
+	snprintf(ptr,_POSIX_PATH_MAX,"/bugger");
+
+	/* try opening with O_EXCL and if it succeeds zero the memory
+	 * by truncating to 0 */
+	if ((fd = shm_open(ptr, O_CREAT|O_RDWR|O_EXCL,
+		S_IRWXU|S_IRWXG|S_IRWXO)) > 0) {
+	/* truncate file to length PCI device's memory */
+	if (ftruncate(fd, HWID_TABLE_SIZE*1024) != 0)
+		FATAL("could not truncate shared file\n");
+	}
+#endif
 	if(argc==2)
 		init();	
 	else
-		lock(atoi(argv[2]));	
+		lock(atoi(argv[2]),atoi(argv[3]));	
 }
+
+
+/*
+ * From /proc/meminfo
+ * MemFree:         2617084 kB
+ *
+ * We could also use memfree from proc/meminfo and feed it in here
+ * 	The effect would be to fall below the acceptable treshold of
+ * 	kswapd and start evicting pages
+ * 	After every iteration the MemFree will increase because kswapd free only chunk
+ * 	at a time until the high water mark is reached again.
+ *
+ */
+
+
+/*
+ * Need to find the limit where kswapd will not have any room to breadth
+ * One way is when chewmem has all the CPU is means kswapd doesn't run
+ *
+ * Another way would be to use a convergence algo when idle ( the memory needed to
+ * survived )
+ *
+ */
+int chewmem_main(int argc, char **argv)
+{
+	int i;
+	char cmd[256];
+	struct pages *page_array;
+	unsigned long long page_nr,size,kb,page_ctrl_nr;
+
+	if(PAGE_SIZE != getpagesize())
+		return -1;
+	
+	if (argc < 2)
+		return 1;
+	kb = atoi(argv[1]);
+	if (kb <= 0)
+		return -1;
+	
+	page_nr = PAGE_ALIGN(kb*1024) / getpagesize();
+	page_ctrl_nr = (PAGE_ALIGN(page_nr*sizeof(struct pages)))/getpagesize();
+
+	page_array = malloc(page_ctrl_nr*getpagesize());
+	if(!page_array)
+		return -1;
+
+	page_nr = page_nr - page_ctrl_nr;
+	if(page_nr ==0)
+		return -1;
+
+	size = (page_ctrl_nr + page_nr) * getpagesize();
+
+	fprintf(stderr,"Req size=%LdkB; Ctrl pages=%Ld; pages=%Ld; Total size %LdkB \n",
+		kb, page_ctrl_nr, page_nr, (size/1024));
+
+	while(1){
+		for (i=0; i<page_nr; i++) {
+			page_array[i].page = malloc(getpagesize());
+			if(!page_array[i].page)
+				return -1;
+			page_array[i].page[0] = 1;
+		}
+		fprintf(stderr,"Allocation completed\n");
+		while(1)sleep(1);
+		sprintf(cmd,"cat /proc/meminfo  |grep -e \"MemTotal\" -e \"MemFree\"\n");
+		system(cmd);
+		sleep(1);
+		for (i=0; i<page_nr; i++) {
+			free(page_array[i].page);
+		}
+		fprintf(stderr,"Freeing up DONE\n");
+	}
+}
+
